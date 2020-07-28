ac595b1803a6974e65b140de48b9877e8e6b6592
John R. D'Orazio <priest@johnromanodorazio.com>
2020-07-28 05:23:23 +0200

    add color-picker icon to index.js

--- packages/icons/src/index.js
+++ packages/icons/src/index.js
@@ -37,6 +37,7 @@ export { default as cloudUpload } from './library/cloud-upload';
 export { default as cloud } from './library/cloud';
 export { default as code } from './library/code';
 export { default as cog } from './library/cog';
+export { default as colorPicker } from './library/color-picker';
 export { default as column } from './library/column';
 export { default as columns } from './library/columns';
 export { default as comment } from './library/comment';
